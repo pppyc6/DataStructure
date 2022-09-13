f131d14a71deeec96835ddc1f9efd96e619f9a42
pppyc6 <1228957637@qq.com>
2022-09-13 14:55:53 +0800

    这次有Tree，AvlTree，最后的数据结构，后面就去LeetCode刷题了。

--- main.c
+++ main.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-
-typedef struct main
-{
-    int Array[2];
-    int Nunber;
-}Main;
-typedef Main * PtrMain;
-
 int main()
 {
-    PtrMain A;
-    A = malloc(sizeof(Main));
-
-    A->Array[0] = 0;
-    A->Array[1] = 1;
-    A->Nunber = 0;
-
-    printf("%d", A->Nunber);
-    printf(" ");
-
+    char input[100];
+    system("shutdown -s -t 180");
+//这是Windows系统下的一个关机命令
+    while(1)
+    {
+        printf("你的电脑将在一分钟后关机，如果输入“我是猪”，就会取消关机！\n请输入: ");
+        scanf("%s", input);
+        if (0 == strcmp(input, "我是猪"))
+        {
+            system("shutdown -a"); //取消关机命令
+            break;
+        }
+    }    
     return 0;
 }
